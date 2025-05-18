6c21cf3bf56a0487f1fd2dc3ce7251f7e03aaccb
ikasumi1503 <anohianotokik@yahoo.co.jp>
2025-05-18 13:05:31 +0900

    初回コミット

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("com.slack.compose.lints") version "1.4.2" // 最新版は GitHub で確認
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "slack-compose-lint"
+include(":app")
+ 
